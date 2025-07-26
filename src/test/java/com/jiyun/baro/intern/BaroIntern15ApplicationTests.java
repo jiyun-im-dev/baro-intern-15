4baf4d329da3ea41729eef7f2e9f2adeff71b8c5
jiyun-im-dev <jiyun.im.dev@gmail.com>
2025-07-26 13:48:23 +0900

    build: first commit

---
+++ src/test/java/com/jiyun/baro/intern/BaroIntern15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiyun.baro.intern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaroIntern15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
